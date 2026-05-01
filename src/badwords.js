8aa054642a0b3325a6cecab721eb365249b70a31
혜정 <jung5318@gmail.com>
2026-05-01 15:39:49 +0900

    리팩토링 및 욕설 추가

--- src/badwords.js
+++ src/badwords.js
@@ -1,4 +1,3 @@
-// const badWords = require("./badwords.ko.config").badWords;
 const { badWords } = require("./badwords.ko.config");
 
 class Filter {
@@ -7,20 +6,19 @@ class Filter {
    * @constructor
    * @param {object} options - Filter instance options
    * @param {boolean} options.emptyList - Instantiate filter with no blacklist
-   * @param {array} options.list - Instantiate filter with custom list
-   * @param {string} options.placeHolder - Character used to replace profane words.
-   * @param {string} options.regex - Regular expression used to sanitize words before comparing them to blacklist.
-   * @param {string} options.replaceRegex - Regular expression used to replace profane words with placeHolder.
-   * @param {string} options.splitRegex - Regular expression used to split a string into words.
+   * @param {string[]} options.list - Additional words to add to blacklist
+   * @param {string[]} options.exclude - Words to exclude from blacklist (whitelist)
+   * @param {string} options.placeHolder - Character used to replace profane words (default: "*")
+   * @param {RegExp} options.replaceRegex - Regex to match characters for replacement (default: /[\s\S]/g)
+   * @param {RegExp} options.splitRegex - Regex to split string into words (default: /\s/)
    */
   constructor(options = {}) {
     this.options = {
       list: options.emptyList ? [] : [...badWords, ...(options.list || [])],
       exclude: options.exclude || [],
-      splitRegex: options.splitRegex || /\s/,
+      splitRegex: options.splitRegex || /(\s+)/,
       placeHolder: options.placeHolder || "*",
-      regex: options.regex || /[^a-zA-Z0-9|$|@]|^/g,
-      replaceRegex: options.replaceRegex || /\w/g,
+      replaceRegex: options.replaceRegex || /[\s\S]/g,
     };
   }
 
@@ -30,23 +28,19 @@ class Filter {
    */
   isProfane(string) {
     const { exclude, list } = this.options;
-
-    return list.some((word) => {
-      const wordExp = new RegExp(word.trim(), "g");
-      return !exclude.includes(word) && wordExp.test(string);
-    });
+    return list.some(
+      (word) =>
+        !exclude.includes(word) && new RegExp(word.trim(), "gi").test(string),
+    );
   }
 
   /**
-   * Replace a word with placeHolder characters;
+   * Replace a word with placeHolder characters.
    * @param {string} string - String to replace.
    */
   replaceWord(string) {
-    const { regex, replaceRegex, placeHolder } = this.options;
-
-    return string
-      .replace(regex, placeHolder)
-      .replace(replaceRegex, placeHolder);
+    const { placeHolder, replaceRegex } = this.options;
+    return string.replace(replaceRegex, placeHolder);
   }
 
   /**
@@ -55,37 +49,33 @@ class Filter {
    */
   clean(string) {
     const { splitRegex } = this.options;
-
     return string
       .split(splitRegex)
       .map((word) => (this.isProfane(word) ? this.replaceWord(word) : word))
-      .join(" ");
+      .join("");
   }
 
   /**
    * Add word(s) to blacklist filter / remove words from whitelist filter
-   * @param {...string} word - Word(s) to add to blacklist
+   * @param {...string} wordsToAdd - Word(s) to add to blacklist
    */
   addWords(...wordsToAdd) {
     const { list, exclude } = this.options;
-
     list.push(...wordsToAdd);
     wordsToAdd.forEach((word) => {
-      const index = exclude.indexOf(word);
-      if (index !== -1) {
-        exclude.splice(index, 1);
-      }
+      const idx = exclude.indexOf(word);
+      if (idx !== -1) exclude.splice(idx, 1);
     });
   }
 
   /**
    * Add words to whitelist filter
-   * @param {...string} word - Word(s) to add to whitelist.
+   * @param {...string} wordsToRemove - Word(s) to add to whitelist.
    */
   removeWords(...wordsToRemove) {
-    const { exclude } = this.options;
-
-    exclude.push(...wordsToRemove.map((word) => word.toLowerCase()));
+    this.options.exclude.push(
+      ...wordsToRemove.map((word) => word.toLowerCase()),
+    );
   }
 }
 
